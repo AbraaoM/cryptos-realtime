a8a1b6c74b6c5de426d076dfcfc5f1879578d824
Abraão Moreira <abraaol.moreira@outlook.com>
2022-01-29 19:36:18 -0300

    calcula candle de 1 minuto

--- api/src/app/app.py
+++ api/src/app/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 import requests
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -19,8 +20,64 @@ class Cryptos(db.Model):
   High = db.Column(db.Float)
   Close = db.Column(db.Float)
 
-req: dict = requests.get('https://poloniex.com/public?command=returnTicker')
 
-print(req.json()["USDC_BTC"])
+candle1min = {'open': 0.0, 'low': 0.0, 'high': 0.0, 'close': 0.0}
+
+high: float = 0
+low: float = 0
+
+
+def GetCurrency(symbol):
+  req = requests.get('https://poloniex.com/public?command=returnTicker')
+  return req.json()[symbol]
+
+def Set1minHigh(lastPrice: float):
+  if candle1min['high'] < lastPrice:
+    candle1min['high'] = lastPrice
+
+def Set1minLow(lastPrice: float):
+  if candle1min['low'] > lastPrice or candle1min['low'] == 0:
+    candle1min['low'] = lastPrice
+
+def open1min(lastPrice: float):
+  global candle1min
+  candle1min = {'open': 0.0, 'low': 0.0, 'high': 0.0, 'close': 0.0}
+  candle1min['open'] = lastPrice
+
+def close1min(lastPrice: float):
+  candle1min['close'] = lastPrice
+
+
+
+# currency = GetCurrency("USDC_BTC")
+# SetHigh(float(currency["last"]))
+# SetcurrentLow(float(currency["last"]))
+
+# print(high)
+
+while True:
+  currency = GetCurrency("USDC_BTC")
+
+  currentTime = datetime.datetime.now().time()
+
+  Set1minHigh(float(currency['last']))
+  Set1minLow(float(currency['last']))
+
+  if currentTime.second == 0:
+    open1min(currency['last'])
+  if currentTime.second == 59:
+    close1min(currency['last'])
+
+  print(candle1min)
+  # if currentTime.minute % 5 == 0:
+  #   open5min()
+  # if (str(currentTime.minute)[1] == 4 or str(currentTime.minute)[1] == 9) and currentTime.second == 59:
+  #   close5min()
+
+  # if currentTime.minute % 10 == 0:
+  #   open10min()
+  # if (str(currentTime.minute)[1] == 4 or str(currentTime.minute)[1] == 9) and currentTime.second == 59:
+  #   close10min()
+  # if oneMinute()
 
 
